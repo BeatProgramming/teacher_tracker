3725dcd3b4597fa603f65720463d1ce51e84ec49
Juan Carlos González Cabrero <malkomich@gmail.com>
2015-12-26 11:51:09 +0100

    Revert "Fixed: Table enrollment not created."
    This reverts commit 2a16159c2484ea74d32038462afb9368d890c46c.

--- app/src/main/java/beatprogramming/github/com/teacker_tracker/BDHelper.java
+++ app/src/main/java/beatprogramming/github/com/teacker_tracker/BDHelper.java
@@ -33,7 +33,6 @@ public class BDHelper extends SQLiteOpenHelper {
         db.execSQL(ScriptBD.CALIFICACION_SCRIPT);
         db.execSQL(ScriptBD.TAREA_SCRIPT);
         db.execSQL(ScriptBD.HORARIO_SCRIPT);
-        db.execSQL(ScriptBD.MATRICULA_SCRIPT);
 
         //- Inserts predefinidos
         db.execSQL(ScriptBD.INSERT_ASIGNATURA1_SCRIPT);
